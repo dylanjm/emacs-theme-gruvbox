aee181f8bda0a7ed663bb76254488e362e110180
Dylan McDowell <dylanjm@users.noreply.github.com>
2020-07-13 12:18:27 -0700

    modify purple

--- gruvbox-dark-hard-theme.el
+++ gruvbox-dark-hard-theme.el
@@ -51,76 +51,77 @@
 (require 'gruvbox)
 
 (gruvbox-deftheme
- gruvbox-dark-hard
- "A retro-groove colour theme (dark version, hard contrast)"
+  gruvbox-dark-hard
+  "A retro-groove colour theme (dark version, hard contrast)"
 
- ((((class color) (min-colors #xFFFFFF))        ; col 1 GUI/24bit
-   ((class color) (min-colors #xFF)))           ; col 2 Xterm/256
+  ((((class color) (min-colors #xFFFFFF))        ; col 1 GUI/24bit
+     ((class color) (min-colors #xFF)))           ; col 2 Xterm/256
 
-  (gruvbox-dark0_hard      "#1d2021" "#1c1c1c")
-  (gruvbox-dark0           "#282828" "#262626")
-  (gruvbox-dark0_soft      "#32302f" "#303030")
-  (gruvbox-dark1           "#3c3836" "#3a3a3a")
-  (gruvbox-dark2           "#504945" "#4e4e4e")
-  (gruvbox-dark3           "#665c54" "#626262")
-  (gruvbox-dark4           "#7c6f64" "#767676")
+    (gruvbox-dark0_hard      "#1d2021" "#1c1c1c")
+    (gruvbox-dark0           "#282828" "#262626")
+    (gruvbox-dark0_soft      "#32302f" "#303030")
+    (gruvbox-dark1           "#3c3836" "#3a3a3a")
+    (gruvbox-dark2           "#504945" "#4e4e4e")
+    (gruvbox-dark3           "#665c54" "#626262")
+    (gruvbox-dark4           "#7c6f64" "#767676")
 
-  (gruvbox-gray            "#928374" "#8a8a8a")
+    (gruvbox-gray            "#928374" "#8a8a8a")
 
-  (gruvbox-light0_hard     "#ffffc8" "#ffffd7")
-  (gruvbox-light0          "#fdf4c1" "#ffffaf")
-  (gruvbox-light1          "#ebdbb2" "#ffdfaf")
-  (gruvbox-light2          "#d5c4a1" "#bcbcbc")
-  (gruvbox-light3          "#bdae93" "#a8a8a8")
-  (gruvbox-light4          "#a89984" "#949494")
+    (gruvbox-light0_hard     "#ffffc8" "#ffffd7")
+    (gruvbox-light0          "#fdf4c1" "#ffffaf")
+    (gruvbox-light1          "#ebdbb2" "#ffdfaf")
+    (gruvbox-light2          "#d5c4a1" "#bcbcbc")
+    (gruvbox-light3          "#bdae93" "#a8a8a8")
+    (gruvbox-light4          "#a89984" "#949494")
 
-  (gruvbox-bright_red      "#fb4933" "#d75f5f")
-  (gruvbox-bright_green    "#b8bb26" "#afaf00")
-  (gruvbox-bright_yellow   "#fabd2f" "#ffaf00")
-  (gruvbox-bright_blue     "#83a598" "#87afaf")
-  (gruvbox-bright_purple   "#d3869b" "#d787af")
-  (gruvbox-bright_aqua     "#8ec07c" "#87af87")
-  (gruvbox-bright_orange   "#fe8019" "#ff8700")
+    (gruvbox-bright_red      "#fb4933" "#d75f5f")
+    (gruvbox-bright_green    "#b8bb26" "#afaf00")
+    (gruvbox-bright_yellow   "#fabd2f" "#ffaf00")
+    (gruvbox-bright_blue     "#83a598" "#87afaf")
+    (gruvbox-bright_purple   "#d3869b" "#d787af")
+    (gruvbox-bright_aqua     "#8ec07c" "#87af87")
+    (gruvbox-bright_orange   "#fe8019" "#ff8700")
 
-  (gruvbox-faded_red       "#cc241d" "#d75f5f")
-  (gruvbox-faded_green     "#98971a" "#afaf00")
-  (gruvbox-faded_yellow    "#d79921" "#ffaf00")
-  (gruvbox-faded_blue      "#458588" "#87afaf")
-  (gruvbox-faded_purple    "#b16286" "#d787af")
-  (gruvbox-faded_aqua      "#689d6a" "#87af87")
-  (gruvbox-faded_orange    "#d65d0e" "#ff8700")
+    (gruvbox-faded_red       "#cc241d" "#d75f5f")
+    (gruvbox-faded_green     "#98971a" "#afaf00")
+    (gruvbox-faded_yellow    "#d79921" "#ffaf00")
+    (gruvbox-faded_blue      "#458588" "#87afaf")
+    (gruvbox-faded_purple    "#b16286" "#d787af")
+    (gruvbox-faded_aqua      "#689d6a" "#87af87")
+    (gruvbox-faded_orange    "#d65d0e" "#ff8700")
 
-  (gruvbox-dark_red        "#421E1E" "#5f0000")
-  (gruvbox-dark_blue       "#2B3C44" "#000087")
-  (gruvbox-dark_aqua       "#36473A" "#005f5f")
+    (gruvbox-dark_red        "#421E1E" "#5f0000")
+    (gruvbox-dark_blue       "#2B3C44" "#000087")
+    (gruvbox-dark_aqua       "#36473A" "#005f5f")
+    (gruvbox-dark_purple     "#66234d" "#d787af")
 
-  (gruvbox-delimiter-one   "#458588" "#008787")
-  (gruvbox-delimiter-two   "#b16286" "#d75f87")
-  (gruvbox-delimiter-three "#8ec07c" "#87af87")
-  (gruvbox-delimiter-four  "#d65d0e" "#d75f00")
-  (gruvbox-white           "#FFFFFF" "#FFFFFF")
-  (gruvbox-black           "#000000" "#000000")
-  (gruvbox-sienna          "#DD6F48" "#d7875f")
-  (gruvbox-lightblue4      "#66999D" "#5fafaf")
-  (gruvbox-burlywood4      "#BBAA97" "#afaf87")
-  (gruvbox-aquamarine4     "#83A598" "#87af87")
-  (gruvbox-turquoise4      "#61ACBB" "#5fafaf")
+    (gruvbox-delimiter-one   "#458588" "#008787")
+    (gruvbox-delimiter-two   "#b16286" "#d75f87")
+    (gruvbox-delimiter-three "#8ec07c" "#87af87")
+    (gruvbox-delimiter-four  "#d65d0e" "#d75f00")
+    (gruvbox-white           "#FFFFFF" "#FFFFFF")
+    (gruvbox-black           "#000000" "#000000")
+    (gruvbox-sienna          "#DD6F48" "#d7875f")
+    (gruvbox-lightblue4      "#66999D" "#5fafaf")
+    (gruvbox-burlywood4      "#BBAA97" "#afaf87")
+    (gruvbox-aquamarine4     "#83A598" "#87af87")
+    (gruvbox-turquoise4      "#61ACBB" "#5fafaf")
 
-  (gruvbox-bg gruvbox-dark0_hard)
-  (gruvbox-bg_inactive gruvbox-dark0)
-  )
+    (gruvbox-bg gruvbox-dark0_hard)
+    (gruvbox-bg_inactive gruvbox-dark0)
+    )
 
- (custom-theme-set-variables 'gruvbox-dark-hard
-                             `(ansi-color-names-vector
-                               [,gruvbox-dark1
-                                ,gruvbox-bright_red
-                                ,gruvbox-bright_green
-                                ,gruvbox-bright_yellow
-                                ,gruvbox-bright_blue
-                                ,gruvbox-bright_purple
-                                ,gruvbox-bright_aqua
-                                ,gruvbox-light1])
-			     `(pdf-view-midnight-colors '(,gruvbox-light0 . ,gruvbox-bg))))
+  (custom-theme-set-variables 'gruvbox-dark-hard
+    `(ansi-color-names-vector
+       [,gruvbox-dark1
+         ,gruvbox-bright_red
+         ,gruvbox-bright_green
+         ,gruvbox-bright_yellow
+         ,gruvbox-bright_blue
+         ,gruvbox-bright_purple
+         ,gruvbox-bright_aqua
+         ,gruvbox-light1])
+    `(pdf-view-midnight-colors '(,gruvbox-light0 . ,gruvbox-bg))))
 
 ;;;###autoload
 (and load-file-name
